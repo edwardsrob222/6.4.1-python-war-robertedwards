738a489b160d8d7600905fde58efbe6a028c3d3b
edwardsrob222 <edwardsrob222@gmail.com>
2019-10-03 16:29:02 -0400

    removes white space to check git

--- war.py
+++ war.py
@@ -1,17 +1,25 @@
-from random import randint
+from random import shuffle, random
 # Card, Deck, Player, and Game
 
 class Card:
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
-        print()
+
     #check if card is red or black
     def isBlackOrRed(self):
         if self.suit == "hearts" or self.suit == "diamonds":
             return "red"
         else:
             return "black"
+
+    def isNumberCard(self):
+        numberCards = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+        for numberCard in numberCards:
+            if self.value == numberCard:
+                return True
+            else:
+                return False
 
     def isFaceCard(self):
         faceCards = [11, 12, 13]
@@ -21,23 +29,35 @@ class Card:
             else:
                 return False
 
+    def isAceCard(self):
+        aceCards = [14]
+        for aceCard in aceCards:
+            if self.value == aceCard:
+                return True
+            else:
+                return False
+
+
+
 
 class Deck:
-    def __init__(self, ):
-        self. =
-        self. =
-        print()
-
-
-class Player:
     def __init__(self):
-        self. =
-        self. =
-        print()
+        self.deck = []
 
-
-class Game:
-    def __init__(self):
-        self. =
-        self. =
-        print()
+    def isFullDeck(self):
+        suits = ["clubs", "diamonds", "hearts", "spades"]
+        for suit in suits:
+            for value in range(1, 14):
+                self.stack.append(card(suit, value))
+# class Player:
+#     def __init__(self):
+#         self. =
+#         self. =
+#         print()
+#
+#
+# class Game:
+#     def __init__(self):
+#         self. =
+#         self. =
+#         print()
